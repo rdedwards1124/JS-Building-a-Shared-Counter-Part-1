24c56d66b6523ae763edd2c69a325f1876d2bf13
Rashonda Edwards <rashondaedwards@aol.com>
2023-03-19 11:44:59 -0700

    Final

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter')
+    let x = await response.json() // x is now an object
+    console.log(x)           // use this to find what is in this object
+    let countValue = x.value // value was shown when countValue was 0
 
     function increment(){
         countValue++;
